7b2a208a3e14e93ec9665efbef2fec23e02819ce
venkaiah1826 <venkaiahb08@gmail.com>
2025-06-24 15:26:00 +0530

    install cypress

---
+++ cypress/e2e/Amozon/mainPage.cy.js
@@ -0,0 +1,5 @@
+
+it('open Amozon HomePage', () => {
+   
+    cy.visit("https://www.amazon.in/?tag=msndeskabkin-21&ref=pd_sl_myh38910b_e&adgrpid=1324913121555467&hvadid=82807336470514&hvnetw=o&hvqmt=e&hvbmt=be&hvdev=c&hvlocint=&hvlocphy=158551&hvtargid=kwd-82807957656483:loc-90&hydadcr=5620_2377278&mcid=bf83b4991e2f3eb79d07c8f0ac1b5488");
+  })
